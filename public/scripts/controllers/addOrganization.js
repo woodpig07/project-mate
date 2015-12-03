2ecacb9243fdf6648688e17d94ec8e0a2d44ce7f
Kate Wu <kate.wu@loweprofero.com>
2015-12-03 14:54:43 +0800

    fix: page does not update when add new organization return successful

--- public/scripts/controllers/addOrganization.js
+++ public/scripts/controllers/addOrganization.js
@@ -5,21 +5,32 @@
     .module('projectMateApp')
     .controller('AddOrganizationCtrl',AddOrganizationCtrl);
 
-  AddOrganizationCtrl.$inject = ['Session','Organizations','$log'];
+  AddOrganizationCtrl.$inject = ['Session','Organizations', 'notifications', '$log'];
 
-  function AddOrganizationCtrl(Session, Organizations,$log) {
+  function AddOrganizationCtrl(Session, Organizations, notifications, $log) {
     var vm = this;
+    vm.loading = false;
 
     vm.addNewOrg = function(newOrg) {
+      vm.loading = true;
+
       Organizations.addOrg(newOrg)
           .then(function(res){
             $log.debug('AddOrganizationCtrl -> addNewOrg() -> Organizations.addOrg()');
             $log.debug(res);
+            var addedOrg = {orgId: res._id, orgName: res.orgName, orgRole: "admin"};
+            Session.user.organizations.push(addedOrg);
+            vm.resetForm(newOrg);
 
+            notifications.showSuccess({message: 'Your organization posted successfully'});
           },
           function(error){
             $log.debug('AddOrganizationCtrl -> addNewOrg() -> Organizations.addOrg() :: ERROR');
             $log.debug(error);
+            notifications.showError({message: "something wrong when adding this organization" + error.data.message});
+          })
+          .finally(function() {
+            vm.loading = false;
           });
     };
 
